f3cb4afc7c36def79bf167360e592733ee575f8a
SaraMadrid11 <157063788+SaraMadrid11@users.noreply.github.com>
2024-06-19 15:51:46 +0100

    Add files via upload

---
+++ Canarian Bliss Paths Web/js files/index.js
@@ -0,0 +1,9 @@
+
+const menuIcon = document.getElementById("menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("open");
+    menuIcon.classList.toggle('bx-x');
+});
+
